120ccbc44e91bb4a250f270bf4746c16ec4a7a0b
SambaGueye <59372751+SambaGueye@users.noreply.github.com>
2022-04-18 15:47:04 +0200

    Ajout de la fonction direPrenom
    Cette fonction prend en entrée un prenom et affiche le prenom de la personne

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log('hello world')
 
 console.log('cette ligne de code est ajoutée depuis la branch develop/samba')
+
+
+function direPrenom(prenom){
+    console.log("je m'appelle "+prenom)
+}
+
