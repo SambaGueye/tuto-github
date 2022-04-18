462464fdd37c727595d434e5c85c5b1b9425b7cf
SambaGueye <59372751+SambaGueye@users.noreply.github.com>
2022-04-18 16:06:08 +0200

    test conflit main

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('hello world')
 
 console.log('cette ligne de code est ajoutée depuis la branch develop/samba')
 
+console.log('testons le conflit')
 
 function direPrenom(prenom){
     console.log("je m'appelle "+prenom)
